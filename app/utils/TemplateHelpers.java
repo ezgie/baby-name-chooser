f5a53d181ef259fefcc3880f07b63b24c6af6ea1
Ezgi Saral <ezgierdag@gmail.com>
2015-06-12 14:18:13 +0200

    cleans code

--- app/utils/TemplateHelpers.java
+++ app/utils/TemplateHelpers.java
@@ -12,19 +12,16 @@ public class TemplateHelpers {
     public static int SHOW_COUNT = 10;
 
     public static List<Integer> createRange(Integer page, Integer pageCount) {
-        int minNumbering = getMinPage(page, pageCount);
+        int minNumbering = getMinPage(page);
         int maxNumbering = getMaxPage(page, pageCount);
-
-//        List<Integer> range = IntStream.range(minNumbering, maxNumbering).boxed().collect(Collectors.toList());
         return ContiguousSet.create(Range.closed(minNumbering, maxNumbering), DiscreteDomain.integers()).asList();
-//        return range;
     }
 
-    public static Integer getMinPage(Integer page, Integer pageCount) {
+    public static Integer getMinPage(Integer page) {
         return (page - MIDDLE) < 1 ? 1 : (page - MIDDLE);
     }
     public static Integer getMaxPage(Integer page, Integer pageCount) {
-        int minNumbering = getMinPage(page, pageCount);
+        int minNumbering = getMinPage(page);
         return (minNumbering + SHOW_COUNT) > pageCount ? pageCount : (minNumbering + SHOW_COUNT);
     }
 }
